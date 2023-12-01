b922337189915578c2ec7a3ee8e92eb1248dc802
Samylly <127408350+Samylly04@users.noreply.github.com>
2023-12-01 13:14:04 -0400

    Add files via upload
    Crud Samylly 01/12

---
+++ CRUD/CRUD2ASamylly/CRUD2ASamylly/BLL/PessoaBLL.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRUD2AJoaoPhelipe.DAL;
+using System.Data;
+using CRUD2AJoaoPhelipe.Model;
+
+namespace CRUD2AJoaoPhelipe.BLL
+{
+    public class PessoaBLL
+    {
+        PessoaDAL pessoaDAL = new PessoaDAL();
+
+        //metodo para salvar 
+        public void Salvar(Pessoa pessoa)
+        {
+            try
+            {
+                pessoaDAL.Salvar(pessoa);
+            }
+            catch (Exception erro) 
+            {
+                throw erro;
+            }
+        }
+
+        //metodo para listar
+
+        public DataTable Listar()
+        {
+          try
+            {
+                DataTable dt = new DataTable();
+                dt = pessoaDAL.Listar();
+                return dt;
+            }
+            catch (Exception erro) 
+            {
+                throw erro;
+            }
+        }
+           
+
+    }
+}
